4c83fb299879f5d7cf8cfa986baab6687fb84e25
Daniel Song <xdtsong@gmail.com>
2017-02-25 15:43:44 -0800

    Add MiniURL model

--- src/shortener/admin.py
+++ src/shortener/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here
+from .models import MiniURL
+
+admin.site.register(MiniURL)
--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+'''	
+	Run these two after any changes here:
+		1) python manage.py makemigrations
+		2) python manage.py migrate
+'''
+
 # Create your models here.
+
+class MiniURL(models.Model):
+	url = models.CharField(max_length=220, )
+
+	def __str__(self):
+		return str(self.url)
+
+	def __unicode__(self):
+		return str(self.url)
