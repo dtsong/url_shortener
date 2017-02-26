c8b73b15a2cbf8d0f2b6e8d63da9cb649cdd6cc3
Daniel Song <xdtsong@gmail.com>
2017-02-25 16:26:29 -0800

    Added timestamp fields

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -11,7 +11,9 @@ from django.db import models
 class MiniURL(models.Model):
 	url = models.CharField(max_length=220, )
 	shortcode = models.CharField(max_length=15, unique=True)
-
+	updated = models.DateTimeField(auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	
 	def __str__(self):
 		return str(self.url)
 
