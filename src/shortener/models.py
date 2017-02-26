b81e3aec2f2fe9e6c985bcad0ac54b7606fc988c
Daniel Song <xdtsong@gmail.com>
2017-02-25 16:19:27 -0800

    Add shortcode attribute to miniUrl model

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -10,6 +10,7 @@ from django.db import models
 
 class MiniURL(models.Model):
 	url = models.CharField(max_length=220, )
+	shortcode = models.CharField(max_length=15, unique=True)
 
 	def __str__(self):
 		return str(self.url)
