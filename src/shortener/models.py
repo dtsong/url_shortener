4c62e63ac7581c27c7e0ad205240456c96d8f13b
Daniel Song <xdtsong@gmail.com>
2017-02-28 15:01:12 -0800

    Add model manager to refresh url shortcodes

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,11 +2,30 @@ from django.db import models
 
 from .utils import code_generator, create_shortcode
 
+class MiniURLManager(models.Manager):
+	def all(self, *args, **kwargs):
+		qs_main = super(MiniURLManager, self).all(*args, **kwargs)
+		qs = qs_main.filter(active=True)
+		return qs
+
+	def refresh_shortcodes(self):
+		qs = MiniURL.objects.filter(id__gte=1)
+		new_codes = 0
+		for q in qs:
+			q.shortcode = create_shortcode(q)
+			print(q.shortcode)
+			q.save
+			new_codes += 1
+		return "New Codes made: {i}".format(i=new_codes)
+
 class MiniURL(models.Model):
 	url = models.CharField(max_length=220, )
 	shortcode = models.CharField(max_length=15, unique=True, blank=True)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
+	active = models.BooleanField(default=True)
+
+	objects = MiniURLManager() # This hooks into the MiniURLManager's method
 	
 	def save(self, *args, **kwargs):
 		if self.shortcode is None or self.shortcode == "":
