3f0fe94cbfca456b288c55b2834f3cd26d2adf3a
Daniel Song <xdtsong@gmail.com>
2017-02-28 14:46:01 -0800

    Move code_generator to utils and make shortcode generation dynamic

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,30 +1,16 @@
 from django.db import models
-import random
-import string
 
-'''	
-	Run these two after any changes here:
-		1) python manage.py makemigrations
-		2) python manage.py migrate
-'''
-
-# Create your models here.
-
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-	# new_code = ""
-	# for _ in range(size):
-	# 	new_code += random.choice(chars)
-	# return new_code
-	return ''.join(random.choice(chars) for _ in range(size))
+from .utils import code_generator, create_shortcode
 
 class MiniURL(models.Model):
 	url = models.CharField(max_length=220, )
-	shortcode = models.CharField(max_length=15, unique=True)
+	shortcode = models.CharField(max_length=15, unique=True, blank=True)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	
 	def save(self, *args, **kwargs):
-		self.shortcode = code_generator()
+		if self.shortcode is None or self.shortcode == "":
+			self.shortcode = create_shortcode(self)
 		super(MiniURL, self).save(*args, *kwargs)
 
 	def __str__(self):
