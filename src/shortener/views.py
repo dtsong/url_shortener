32e86cf55fd2912757f4a0482666f06c3a821394
Daniel Song <xdtsong@gmail.com>
2017-02-28 15:20:36 -0800

    Add initial views

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+def mini_redirect_view(request, *args, **kwargs):
+	return HttpResponse("hello")
+
+class MiniRedirectView(View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("hello again")
