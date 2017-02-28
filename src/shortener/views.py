9782770ab084a3895e7d534e45d851d0be949c9c
Daniel Song <xdtsong@gmail.com>
2017-02-28 15:53:23 -0800

    Add 404 implementation

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .models import MiniURL
+
 def mini_redirect_view(request, shortcode=None, *args, **kwargs): #FBV
-	return HttpResponse("hello {sc}".format(sc=shortcode))
+	obj = get_object_or_404(MiniURL, shortcode=shortcode)
+	return HttpResponse("hello {sc}".format(sc=obj.url))
 
 
 class MiniCBVView(View): #CBV
 	def get(self, request, shortcode=None, *args, **kwargs):
-		return HttpResponse("hello again {sc}".format(sc=shortcode))
+		obj = get_object_or_404(MiniURL, shortcode=shortcode)
+		return HttpResponse("hello again {sc}".format(sc=obj.shortcode))
+
