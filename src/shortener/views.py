c316f1d734b6552a162505129defc2928bd1024a
Daniel Song <xdtsong@gmail.com>
2017-02-28 16:48:08 -0800

    Allow URL to redirect and change URLs to handle character checking

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -6,11 +6,10 @@ from .models import MiniURL
 
 def mini_redirect_view(request, shortcode=None, *args, **kwargs): #FBV
 	obj = get_object_or_404(MiniURL, shortcode=shortcode)
-	return HttpResponse("hello {sc}".format(sc=obj.url))
+	return HttpResponseRedirect(obj.url)
 
 
 class MiniCBVView(View): #CBV
 	def get(self, request, shortcode=None, *args, **kwargs):
 		obj = get_object_or_404(MiniURL, shortcode=shortcode)
-		return HttpResponse("hello again {sc}".format(sc=obj.shortcode))
-
+		return HttpResponseRedirect(obj.url)
