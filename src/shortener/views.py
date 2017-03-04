1d65896ec2bafc65acc4d43491876506e6466f37
Daniel Song <xdtsong@gmail.com>
2017-03-04 15:48:34 -0800

    Add HTML form

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -5,8 +5,16 @@ from django.views import View
 from .models import MiniURL
 
 
+def home_view_fbv(request, *args, **kwargs):
+	if request.method == "POST":
+		print(request.POST)
+	return render(request, "shortener/home.html", {})
+
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
+		return render(request, "shortener/home.html", {})
+
+	def post(self, request, *args, **kwargs):
 		return render(request, "shortener/home.html", {})
 
 
